0e58c49f3e266387551d0f32443f9768abb98e44
Rifai <rifaigame29@gmail.com>
2020-11-26 14:00:59 +0700

    Edit v2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Test Node Deployment by faidancode' });
+  res.render('index', { title: 'Test Node Deployment by faidancode v-2' });
 });
 
 module.exports = router;
